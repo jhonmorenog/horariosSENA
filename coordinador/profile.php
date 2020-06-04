56d8f9fbf79892883f823389358bb5c80a50127b
Jhon Moreno <Jhon Moreno@192.168.0.12>
2020-06-04 14:41:26 -0500

    anerso archivos para la automatización

--- coordinador/profile.php
+++ coordinador/profile.php
@@ -1,8 +1,6 @@
 <?php
+require_once '../utili/Conexion.php';
+require_once '../ScriptDB/Querys.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+?>
 
