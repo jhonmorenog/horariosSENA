a8815dbbe3a14c1ae9f1d6c4f378d9042367cf69
Familia <Familia@DESKTOP-ES0BNM8>
2020-06-17 15:27:03 -0500

    habilitar aula funcional

--- administrador/Habilitar/HabilitarAula.php
+++ administrador/Habilitar/HabilitarAula.php
@@ -4,7 +4,7 @@ require_once "../../utili/Conexion.php";
 $id_aula = $_POST['idh'];
 
 
-$query = "UPDATE aula set  estado_aula = 0 WHERE id_aula=$id_aula";
+$query = "UPDATE aula set  estado = 0 WHERE id_aula=$id_aula";
 if($mysqli->query($query)){
 	print"<script>alert(\"Actualizacion exitosa.\");window.location='../aulas.php';</script>";
 }else {
