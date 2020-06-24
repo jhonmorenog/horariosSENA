833d763081bbd2542a319face38b73b2584430d5
dinam <dinam@LAPTOP-KRTQA3LB>
2020-06-24 14:26:26 -0500

    Correcion Vendors y SESSION

--- coordinador/production/horarios2.php
+++ coordinador/production/horarios2.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+      <?php
+        require_once '../../utili/sesion.php';
+        ?>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
@@ -16,7 +19,7 @@
     <!-- NProgress -->
     <link href="../../vendor/nprogress/nprogress.css" rel="stylesheet">
     <!-- bootstrap-daterangepicker -->
-    <link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link href="../vendor/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
     <!-- Custom Theme Style -->
     <link href="../../build/css/custom.min.css" rel="stylesheet">
@@ -40,7 +43,7 @@
               </div>
               <div class="profile_info">
                 <span>Bienvenido,</span>
-                <h2>Eddie Merck Camacho</h2>
+                <h2><?php echo $_SESSION['user'] ?></h2>
               </div>
             </div>
             <!-- /menu profile quick info -->
@@ -105,7 +108,7 @@
                 <ul class=" navbar-right">
                   <li class="nav-item dropdown open" style="padding-left: 15px;">
                     <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                      <img src="images/img.jpg" alt="">Eddie Merck Camacho
+                      <img src="images/img.jpg" alt=""><?php echo $_SESSION['user'] ?>
                     </a>
                     <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
                       <a class="dropdown-item"  href="profile.php"> Perfil</a>
@@ -319,24 +322,24 @@
     <!-- NProgress -->
     <script src="../../vendor/nprogress/nprogress.js"></script>
     <!-- Chart.js -->
-    <script src="../../vendors/Chart.js/dist/Chart.min.js"></script>
+    <script src="../../vendor/Chart.js/dist/Chart.min.js"></script>
     <!-- jQuery Sparklines -->
-    <script src="../../vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+    <script src="../../vendor/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
     <!-- Flot -->
-    <script src="../../vendors/Flot/jquery.flot.js"></script>
-    <script src="../../vendors/Flot/jquery.flot.pie.js"></script>
-    <script src="../../vendors/Flot/jquery.flot.time.js"></script>
-    <script src="../../vendors/Flot/jquery.flot.stack.js"></script>
-    <script src="../../vendors/Flot/jquery.flot.resize.js"></script>
+    <script src="../../vendor/Flot/jquery.flot.js"></script>
+    <script src="../../vendor/Flot/jquery.flot.pie.js"></script>
+    <script src="../../vendor/Flot/jquery.flot.time.js"></script>
+    <script src="../../vendor/Flot/jquery.flot.stack.js"></script>
+    <script src="../../vendor/Flot/jquery.flot.resize.js"></script>
     <!-- Flot plugins -->
-    <script src="../../vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-    <script src="../../vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-    <script src="../../vendors/flot.curvedlines/curvedLines.js"></script>
+    <script src="../../vendor/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="../../vendor/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="../../vendor/flot.curvedlines/curvedLines.js"></script>
     <!-- DateJS -->
-    <script src="../../vendors/DateJS/build/date.js"></script>
+    <script src="../../vendor/DateJS/build/date.js"></script>
     <!-- bootstrap-daterangepicker -->
     <script src="../../vendor/moment/min/moment.min.js"></script>
-    <script src="../../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <script src="../../vendor/bootstrap-daterangepicker/daterangepicker.js"></script>
     
     <!-- Custom Theme Scripts -->
     <script src="../../build/js/custom.min.js"></script>
