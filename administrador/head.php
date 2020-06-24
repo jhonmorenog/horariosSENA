d5dd775aafa96431c226496914eda6d43e5a8a2d
Hewlett Packard <Hewlett Packard@DESKTOP-6S9PRIS>
2020-06-23 19:22:06 -0500

    se cambio de nombre de la carpeta vendors y se redirigieron los links

--- administrador/head.php
+++ administrador/head.php
@@ -9,16 +9,16 @@
 
     <!-- Bootstrap -->
     <link href="cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
-    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../otro/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../otro/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- NProgress -->
-    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <link href="../otro/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
-    <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <link href="../otro/iCheck/skins/flat/green.css" rel="stylesheet">
     <!-- Datatables -->
     
-    <link href="../vendors/datatables.net/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="../otro/datatables.net/css/dataTables.bootstrap.min.css" rel="stylesheet">
     <link rel="shortcut icon" type="image/x-icon" href="icon/Horarios.ico" />
     <!-- Custom Theme Style -->
     <link href="../build/css/custom.min.css" rel="stylesheet">
