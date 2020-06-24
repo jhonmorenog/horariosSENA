fbb06278e771d30069747c12605049d72af0ba80
Hewlett Packard <Hewlett Packard@DESKTOP-6S9PRIS>
2020-06-23 19:45:00 -0500

    se cambio el nomnbre a vendors

--- administrador/archivos_js.php
+++ administrador/archivos_js.php
@@ -1,21 +1,21 @@
 <html>
     <!-- jQuery -->
-    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <script src="../vendor/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
-   <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="../vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <!-- FastClick -->
-    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <script src="../vendor/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
-    <script src="../vendors/nprogress/nprogress.js"></script>
+    <script src="../vendor/nprogress/nprogress.js"></script>
     <!-- iCheck -->
-    <script src="../vendors/iCheck/icheck.min.js"></script>
+    <script src="../vendor/iCheck/icheck.min.js"></script>
     <!-- Datatables -->
-    <script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="../vendors/datatables.net/js/dataTables.bootstrap.min.js"></script>
+    <script src="../vendor/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables.net/js/dataTables.bootstrap.min.js"></script>
    
-    <script src="../vendors/jszip/dist/jszip.min.js"></script>
-    <script src="../vendors/pdfmake/build/pdfmake.min.js"></script>
-    <script src="../vendors/pdfmake/build/vfs_fonts.js"></script>
+    <script src="../vendor/jszip/dist/jszip.min.js"></script>
+    <script src="../vendor/pdfmake/build/pdfmake.min.js"></script>
+    <script src="../vendor/pdfmake/build/vfs_fonts.js"></script>
 
     <!-- Custom Theme Scripts -->
     <script src="../build/js/custom.min.js"></script>
